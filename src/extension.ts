ebbed59257ccf398671f1de737a9f4e59ab9d9f7
Pig Fang <g-plane@hotmail.com>
2024-08-30 12:28:52 +0800

    start language server only when config file existed

--- src/extension.ts
+++ src/extension.ts
@@ -6,9 +6,14 @@ export async function activate(context: vscode.ExtensionContext) {
   const outputChannel = vscode.window.createOutputChannel('dprint')
   context.subscriptions.push(outputChannel)
 
-  const client = await setupLspClient(outputChannel)
-  client.start()
-  context.subscriptions.push(client)
+  if (
+    (await vscode.workspace.findFiles('**/{dprint,.dprint}.{json,jsonc}', '**/node_modules/**', 1))
+      .length > 0
+  ) {
+    const client = await setupLspClient(outputChannel)
+    client.start()
+    context.subscriptions.push(client)
+  }
 
   context.subscriptions.push(
     vscode.commands.registerCommand('dprint.configInit', commandConfigInit)
